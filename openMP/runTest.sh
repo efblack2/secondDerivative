efc1344c5f7aa8d7566c322b884ddf6b452409d0
Edgar F. Black <efblack2@illinois.edu>
2018-02-09 14:22:54 -0600

    run scripts updated

--- openMP/runTest.sh
+++ openMP/runTest.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 if [ "$#" -ne 2 ] 
 then
-  echo "Usage: $0 inputFile compiler"
+  echo "Usage: $0 derivativeLoops  compilerResults"
   exit 1
 fi
 
 nloops=5
 np=`grep -c ^processor /proc/cpuinfo`
 
-
-rm -f openMpResult.txt
+rm -f OpenMp_Result.txt
 for i in  `seq 1 $np`; do
     export OMP_NUM_THREADS=$i
     for j in  `seq 1 $nloops`; do
         echo number of threads: $i, run number: $j 
-        p6 $1 | grep finish >>  openMpResult.txt
+        ./secondDerivative $1 | grep finish >>  OpenMp_Result.txt
     done
 done
 
-mkdir -p ../../../plots/PureShareMemory/$(hostname)/$2
-cat openMpResult.txt | awk '{}{print $6, $3}{}' | awk '{Prod[$1]++; min[$1]=Prod[$1]==1||min[$1]>$2?$2:min[$1]} END{ for (var in Prod) printf "%s threads: the min is %f\n", var,min[var]}'  | sort -n  > ../../../plots/PureShareMemory/$(hostname)/$2/p6_OpenMP.txt
+mkdir -p ../../plots/$(hostname)/$2
+cat OpenMp_Result.txt | awk '{}{print $6, $3}{}' | awk '{Prod[$1]++; min[$1]=Prod[$1]==1||min[$1]>$2?$2:min[$1]} END{ for (var in Prod) printf "%s threads: the min is %f\n", var,min[var]}'  | sort -n  > ../../plots/$(hostname)/$2/OpenMP.txt
 
-rm openMpResult.txt
-#rm RunHistory.dat
+rm OpenMp_Result.txt
 
