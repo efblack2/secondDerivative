a1e6e2c56b0ca50beaa7da2ee6bfaf6c6cc44c67
Edgar Black <efblack2@stout.cs.illinois.edu>
2018-02-20 17:51:01 -0600

    fixed running scripts

--- MPI_sm/runTest.sh
+++ MPI_sm/runTest.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-if [ "$#" -ne 2 ] 
+if [ "$#" -ne 1 ]
 then
-  echo "Usage: $0 derivativeLoops  compilerResults"
+  echo "Usage: $0  compilerResults"
   exit 1
 fi
 
-#nloops=1
+nloops=5
 np=`grep -c ^processor /proc/cpuinfo`
 #np=4
 
 rm -f Mpi_sm_Result.txt
 for i in  `seq 1 $np`; do
 
-    #for j in  `seq 1 $nloops`; do
+    for j in  `seq 1 $nloops`; do
         echo number of processors: $i
-        mpiexec -n $i ./secondDerivative $1 | grep finish >>  Mpi_sm_Result.txt
-    #done
+        mpiexec -n $i ./secondDerivative  | grep finish >>  Mpi_sm_Result.txt
+    done
 done
 
-mkdir -p ../../plots/$(hostname)/$2
-cat Mpi_sm_Result.txt | awk '{}{print $2, $6, $11, $16}{}'  > ../../plots/$(hostname)/$2/MPI_sm.txt
+mkdir -p ../../plots/$(hostname)/$1
+cat Mpi_sm_Result.txt | awk '{}{print $2, $6, $11, $16}{}'  > ../../plots/$(hostname)/$1/MPI_sm.txt
 
 rm Mpi_sm_Result.txt
 
--- OpenMP/runTest.sh
+++ OpenMP/runTest.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-if [ "$#" -ne 2 ] 
+if [ "$#" -ne 1 ]
 then
-  echo "Usage: $0 derivativeLoops  compilerResults"
+  echo "Usage: $0  compilerResults"
   exit 1
 fi
 
-#nloops=1
+nloops=3
 np=`grep -c ^processor /proc/cpuinfo`
 #np=4
 
 rm -f OpenMp_Result.txt
 for i in  `seq 1 $np`; do
     export OMP_NUM_THREADS=$i
-    #for j in  `seq 1 $nloops`; do
+    for j in  `seq 1 $nloops`; do
         echo number of threads: $i
-        ./secondDerivative $1 | grep finish >>  OpenMp_Result.txt
-    #done
+        ./secondDerivative  | grep finish >>  OpenMp_Result.txt
+    done
 done
 
-mkdir -p ../../plots/$(hostname)/$2
-cat OpenMp_Result.txt | awk '{}{print $2, $6, $11, $16}{}'  > ../../plots/$(hostname)/$2/OpenMP.txt
+mkdir -p ../../plots/$(hostname)/$1
+cat OpenMp_Result.txt | awk '{}{print $2, $6+$11+$16, $6, $11, $16}{}'   | sort  -k1,1n -k2,2n   | awk 'BEGIN{ prev=-1} { if ($1 != prev) { print $0; prev=$1}  } END{}'  > ../../plots/$(hostname)/$1/OpenMP.txt
 
 rm OpenMp_Result.txt
 
