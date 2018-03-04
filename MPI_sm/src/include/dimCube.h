1cec3965c1bc014028156e99296522092dd94027
Edgar F. Black <edgarfblack@gmail.com>
2018-03-04 08:47:33 -0600

    addinig initializarion to dimCube.h

--- MPI_sm/src/include/dimCube.h
+++ MPI_sm/src/include/dimCube.h
@@ -1,13 +1,13 @@
-#include <mpi.h> 
+#include <mpi.h>
 #include "myMPI.h"
 #include "real.h"
 
 // erase //
-#include <stdio.h> 
-#include <stdlib.h> 
+#include <stdio.h>
+#include <stdlib.h>
 // erase //
 
-real*** dimCube(int level, int row, int col, MPI_Win *sm_win, MPI_Comm *sm_comm) 
+real*** dimCube(int level, int row, int col, MPI_Win *sm_win, MPI_Comm *sm_comm)
 {
 
     int myRank,commSize;
@@ -23,44 +23,61 @@ real*** dimCube(int level, int row, int col, MPI_Win *sm_win, MPI_Comm *sm_comm)
     start = BLOCK_LOW (myRank,commSize,(level));
     end   = BLOCK_HIGH(myRank,commSize,(level));
     locLevSize=1+end-start;
-    
-    
+
+
     //const int size = level * row * col;
     real ***cube;
-      
-    cube       = (real ***)  malloc( locLevSize         * sizeof(real**));        
-    cube[0]    = (real  **)  malloc( locLevSize  * row  * sizeof(real*));     
-    //cube[0][0] = (real   *)  calloc(( high  * row * col),sizeof(real));      
-    
+
+    cube       = (real ***)  malloc( locLevSize         * sizeof(real**));
+    cube[0]    = (real  **)  malloc( locLevSize  * row  * sizeof(real*));
+    //cube[0][0] = (real   *)  calloc(( high  * row * col),sizeof(real));
+
+    MPI_Aint sz;
+    int dispUnit;
+
     MPI_Win_allocate_shared((MPI_Aint) (locLevSize * row * col)*sizeof(real), sizeof(real), myInfo,*sm_comm,&cube[0][0],sm_win);
+    MPI_Win_shared_query(*sm_win, myRank, &sz,&dispUnit,&cube[0][0]);
+
     /*
     if (myRank == 0) {
         MPI_Win_allocate_shared((MPI_Aint) size*sizeof(real), sizeof(real), MPI_INFO_NULL,*sm_comm,&cube[0][0],sm_win);
     } else {
         MPI_Win_allocate_shared((MPI_Aint) 0,                 sizeof(real), MPI_INFO_NULL,*sm_comm,&cube[0][0],sm_win);
     } // end if //
+    MPI_Win_shared_query(*sm_win, MPI_PROC_NULL, &sz,&dispUnit,&cube[0][0]);
     */
-    
-    
-    MPI_Barrier(*sm_comm);  // is this really needed? //
-    
-    MPI_Aint sz;
-    int dispUnit;
-    //MPI_Win_shared_query(*sm_win, MPI_PROC_NULL, &sz,&dispUnit,&cube[0][0]);
-    MPI_Win_shared_query(*sm_win, myRank, &sz,&dispUnit,&cube[0][0]);
-    
+
+
+    // creating the cube array
     for(int l=0; l < locLevSize; ++l){
-        cube[l] = cube[0]  + (l * row); 
+        cube[l] = cube[0]  + (l * row);
         for(int r=0; r < row; ++r){
             cube[l][r] = cube[0][0]  +  (l *  row * col )  + r * col;
         } // end for //
     } // end for //
+    // end of creating the cube array
 
-    MPI_Barrier(*sm_comm);  // is this really needed? //
+    //MPI_Barrier(*sm_comm);  // is this really needed? //
+
+    MPI_Win_lock_all(0,*sm_win);
+
+    for (int l=0; l<locLevSize; ++l){
+        for (int r=0; r<row; ++r){
+            for (int c=0; c<col; ++c){
+                cube[l][r][c] =  (real) 0.0;
+            } // end for //
+        } // end for //
+    } // end for //
+
+    MPI_Win_sync(*sm_win);
+    MPI_Barrier(*sm_comm);
+    MPI_Win_unlock_all(*sm_win);
+    //MPI_Barrier(*sm_comm);  // is this really needed? //
+
 
 /*
     MPI_Win_lock_all(0,*sm_win);
-    
+
     if (myRank==0) {
         for (int l=0; l<level; ++l){
             for (int r=0; r<row; ++r){
@@ -70,15 +87,15 @@ real*** dimCube(int level, int row, int col, MPI_Win *sm_win, MPI_Comm *sm_comm)
             } // end for //
         } // end for //
     } // end if //
-    
+
     MPI_Win_sync(*sm_win);
     MPI_Barrier(*sm_comm);
     MPI_Win_unlock_all(*sm_win);
     MPI_Barrier(*sm_comm);  // is this really needed? //
 */
-      
+
    return cube;
-   
+
 } // end of dimCube() //
 
 void freeCube(real ****cube, MPI_Win *sm_win)
@@ -86,5 +103,5 @@ void freeCube(real ****cube, MPI_Win *sm_win)
     MPI_Win_free(sm_win);
     //free((*cube)[0][0]);
     free((*cube)[0]);
-    free((*cube));     
+    free((*cube));
 } // end of freeCube //
--- OpenMP/src/include/dimCube.h
+++ OpenMP/src/include/dimCube.h
@@ -1,28 +1,40 @@
 #include "real.h"
 
-real*** dimCube(int high, int row, int col) 
+real*** dimCube(int high, int row, int col)
 {
 
     real ***cube;
-      
-    cube       = (real ***)  malloc(  high  * sizeof(real**));        
-    cube[0]    = (real  **)  malloc(  high  * row  * sizeof(real*));     
-    cube[0][0] = (real   *)  malloc(( high  * row * col) * sizeof(real));      
 
+    cube       = (real ***)  malloc(  high  * sizeof(real**));
+    cube[0]    = (real  **)  malloc(  high  * row  * sizeof(real*));
+    cube[0][0] = (real   *)  malloc(( high  * row * col) * sizeof(real));
+
+    // creating the cube array
     for(int k=0; k < high; ++k){
-        cube[k] = cube[0]  + (k * row); 
+        cube[k] = cube[0]  + (k * row);
         for(int j=0; j < row; ++j){
             cube[k][j] = cube[0][0]  +  (k *  row * col )  + j * col;
         } // end for //
     } // end for //
-      
+    // end of creating the cube array
+
+    #pragma omp parallel for
+    for(int l = 0; l < high; ++l) {
+        for(int r = 0; r < row ; ++r) {
+            for(int c = 0; c < col; ++c) {
+                cube[l][r][c] = (real) 0.0;
+            } // end for //
+        } // end for //
+    } // end for //
+
+
    return cube;
-   
+
 } // end of dimCube() //
 
 void freeCube(real ****cube)
 {
     free((*cube)[0][0]);
     free((*cube)[0]);
-    free((*cube));     
+    free((*cube));
 } // end of freeCube //
