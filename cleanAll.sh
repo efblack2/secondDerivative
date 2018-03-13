22afdd8e37f160b3b049725e0462242ecb1bae46
Edgar F. Black <efblack2@illinois.edu>
2018-03-13 09:44:48 -0500

    compile and clean scripts

---
+++ cleanAll.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd OpenMP/
+rm -rf buildGnu  buildIntel  buildPgi
+cd ../MPI_sm/
+rm -rf buildGnu  buildIntel  buildPgi
+cd ..
+
---
+++ compileAll.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+
+mkdir -p  MPI_sm/buildGnu
+mkdir -p  OpenMP/buildGnu
+
+mkdir -p  MPI_sm/buildIntel
+mkdir -p  OpenMP/buildIntel
+
+mkdir -p  MPI_sm/buildPgi
+mkdir -p  OpenMP/buildPgi
+
+
+
+cd OpenMP/buildGnu
+cmake .. ; make clean; make
+cd ../../MPI_sm/buildGnu
+cmake .. ; make clean; make
+cd ../../
+
+export CC=icc
+export CXX=icpc
+source setIcc intel64
+source setImpi
+
+
+cd OpenMP/buildIntel
+cmake .. ; make clean; make
+cd ../../MPI_sm/buildIntel
+cmake .. ; make clean; make
+cd ../../
+
+export CC=pgcc
+export CXX=pgc++
+source setPgi 18.1
+source setPgiMpi 18.10
+
+cd OpenMP/buildPgi
+cmake .. ; make clean; make
+cd ../../MPI_sm/buildPgi
+cmake .. ; make clean; make
+cd ../../
+
