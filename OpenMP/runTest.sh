9ae95c1a990b86d6083fad9b1451b30482cf913e
Edgar F. Black <efblack2@illinois.edu>
2018-03-19 08:49:52 -0500

    imptoving running script

--- OpenMP/runTest.sh
+++ OpenMP/runTest.sh
@@ -5,14 +5,57 @@ then
   exit 1
 fi
 
-nloops=5
-# needed by intel compiler in Blue Waters
-export KMP_AFFINITY=disabled
-# needed by intel compiler in Blue Waters
+nloops=3
+
 
 npt=`grep -c ^processor /proc/cpuinfo`
-np="$(($npt / 1))"
+slots=`numactl -H | grep available | awk '{}{print $2}{}'`
+np="$(($npt / $slots))"
+npps="$(($np / 2))"
+npm1="$(($np - 1))"
 
+sequence=''
+##########################################
+for i in  `seq 0 $((npps-1))`; do
+    sequence+=$i','
+    sequence+=$(($i +  $((np/2))  ))','
+done
+##########################################
+#for i in `seq 0 $((npm1))`; do
+#    sequence+=$i','
+#done
+##########################################
+#for i in `seq 0 2 $((npm1))`; do
+#    sequence+=$i','
+#done
+#for i in `seq 1 2 $((npm1))`; do
+#    sequence+=$i','
+#done
+##########################################
+
+sequence=${sequence%?}
+if [ -n "$LM_LICENSE_FILE" ]; then
+    echo "Pgi Compiler"
+    export MP_BIND="yes"
+    export MP_BLIST=$sequence
+    #export MP_BLIST="0-$npm1"
+    echo $MP_BLIST
+elif [ -n "$INTEL_LICENSE_FILE" ]; then
+    echo "Intel Compiler"
+    export OMP_PLACES=sockets
+    export OMP_PROC_BIND=true
+    #export KMP_AFFINITY=scatter
+    # needed to use dissabled in Blue waters
+    #export KMP_AFFINITY=disabled
+else
+    echo "Gnu Compiler"
+    #export OMP_PLACES=sockets
+    #export OMP_PROC_BIND=true
+    export GOMP_CPU_AFFINITY=$sequence
+    #export GOMP_CPU_AFFINITY="0-$npm1"
+fi
+
+echo $sequence
 
 rm -f OpenMp_Result.txt
 for i in  `seq 1 $np`; do
